b6a335eb8dbf52142ae5b2d1075aa39de4454ef9
PatreceCampbell <patrececampbell5@gmail.com>
2021-11-16 01:08:29 -0500

    Edited Home Screen

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/michel/DoC_Chatbot/venv"
+set -gx VIRTUAL_ENV "/home/patrece/Desktop/DoC_Chatbot/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
