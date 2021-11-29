3a2ef795ab5182dc2051bf5c98e7c493c783fd71
Michel0288 <micheldavies0288@gmail.com>
2021-11-29 14:25:21 -0500

    Changes

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,26 @@
-/* Add your Application JavaScript */
+jQuery(document).ready(function() {
+    $("#SendButton").click(function(e) {
+      e.preventDefault();
+
+
+      $.ajax({
+          type: "POST",
+          url: "/chatbot",
+          data: {
+              question: $("#UserInput").val()
+           
+          },
+          success: function(result) {
+            $("#BotResponse").append("<br><br>Me: "+$("#UserInput").val()+ "<br><br> CompBot: "+result.response)+"<br><br><br>";
+            $("#UserInput").val("")
+          },
+          error: function(result) {
+              alert('error');
+          }
+      });
+
+
+
+    });
+
+  });
